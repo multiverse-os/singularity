92f8c40ed12121213442b4ad6581262e5466ae14
Your Name <you@example.com>
2019-09-19 04:05:39 -0400

    issue was with lack of space between command the arguments, so forced one

--- cmd/singularity/main.go
+++ cmd/singularity/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	"os"
+	//"os"
 
 	singularity "github.com/multiverse-os/singularity"
 	//memexec "github.com/multiverse-os/singularity/memexec"
 	executable "github.com/multiverse-os/singularity/store/executable"
-	vfs "github.com/multiverse-os/singularity/store/vfs"
-	memfs "github.com/multiverse-os/singularity/store/vfs/memfs"
-	mountfs "github.com/multiverse-os/singularity/store/vfs/mountfs"
+	//vfs "github.com/multiverse-os/singularity/store/vfs"
+	//memfs "github.com/multiverse-os/singularity/store/vfs/memfs"
+	//mountfs "github.com/multiverse-os/singularity/store/vfs/mountfs"
 )
 
 func main() {
@@ -18,23 +18,23 @@ func main() {
 	fmt.Println("An example of binary execution completely in memory without ")
 	fmt.Println("touching the disk, or creating temporary files using memFD.\n")
 
-	memoryCommand := singularity.MemoryCommand("rubytest", executable.Ruby)
+	singularity.MemoryCommand("rubytest", executable.Ruby).Run(`-e "hello world from ruby"`)
 
-	osfs := vfs.OS()
-	osfs.Mkdir("/tmp", 0777)
-	fs := mountfs.Create(osfs)
-	mfs := memfs.Create()
-	mfs.Mkdir("/memfs", 0777)
-	fs.Mount(mfs, "/memfs")
-	fs.Mkdir("/memfs/rubyscripts", 0777)
+	//osfs := vfs.OS()
+	//osfs.Mkdir("/tmp", 0777)
+	//fs := mountfs.Create(osfs)
+	//mfs := memfs.Create()
+	//mfs.Mkdir("/memfs", 0777)
+	//fs.Mount(mfs, "/memfs")
+	//fs.Mkdir("/memfs/rubyscripts", 0777)
 
-	memoryFile, _ := osfs.OpenFile("/memfs/rubyscripts/helloworld.rb", os.O_RDWR, 0)
-	_, err := memoryFile.Write([]byte("p 'hello world from ruby temp file'"))
-	if err != nil {
-		fmt.Errorf("[error] failed to write to memory filesystem:\n", err)
-	}
+	//memoryFile, _ := osfs.OpenFile("/memfs/rubyscripts/helloworld.rb", os.O_RDWR, 0)
+	//_, err := memoryFile.Write([]byte("p 'hello world from ruby temp file'"))
+	//if err != nil {
+	//	fmt.Errorf("[error] failed to write to memory filesystem:\n", err)
+	//}
 
-	memoryCommand.Run("-e 'hello world from ruby'")
+	//memoryCommand.Run("-e 'hello world from ruby'")
 
 	//outputBytes, err := memexec.Command(executable.Ruby, " /memfs/rubyscripts/helloworld.rb").Run()
 	//if err != nil {
